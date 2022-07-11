0cd23d351ab071896188b8a5cf252b6c40265e7e
Rashmi <rashmi.sinha1st@gmail.com>
2022-07-11 15:17:14 +0530

    commit on resume-app

---
+++ src/compoments/Nav/NavFloater.js
@@ -0,0 +1,24 @@
+import "./Nav.css";
+
+
+
+function NavFloater(){
+
+
+    return(
+        
+        <div id="stickyNav" className="header2">
+        <div className="rightSide">
+        <div className="links" >
+          <a href="#profileSummary">About</a>
+          <a href="#skillHeader">Skills</a>
+          <a href="#projectHeader">Projects</a>
+          <a href="#educationHeader">Education</a>
+          <a href="#contactMe">Contact Me</a>
+        </div>
+      
+      </div>
+      </div>
+    )
+}
+export default NavFloater;
