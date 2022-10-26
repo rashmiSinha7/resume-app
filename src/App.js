5cf3fc0f1d98aa7d8b36c5f5f44f0d0c24ad8331
Rashmi <rashmi.sinha1st@gmail.com>
2022-10-26 21:22:41 +0530

    commit9

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import NavFloater from "./compoments/Nav/NavFloater";
 function App() {
   return (
     <div className="App">
+      <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
       
       <Header />
       <div>
--- src/compoments/Skill/Skills.js
+++ src/compoments/Skill/Skills.js
@@ -7,32 +7,34 @@ import dbImg from "./database.png";
 import testImg from "./test.png";
 
 class Skill extends React.Component {
+  
   render() {
     return (
       <div>
+   
         <p align="center " id="skillHeader">
           My Skills
         </p>
         <div className="skill">
-          <SkillBox
+          <SkillBox className="skillList"
             skillType="Frameworks and Libraries"
             skill1="React Js"
             skill2="Redux, Hooks"
             skill3="Material UI" skillImg={reactImg}
           />
-          <SkillBox 
+          <SkillBox className="skillList"
           skillType="Languages"
           skill1="Javascript, ES6, TYPESCRIPT, NPM"
           skill2="Core Java, collection FrameWork"
           skill3="HTML& CSS, JSX" skillImg={jsImg}
           />
-          <SkillBox
+          <SkillBox className="skillList"
            skillType="DataBases"
            skill1="MySQL"
            skill2="Oracle"
            skill3="FireBase" skillImg={dbImg}
             />
-          <SkillBox 
+          <SkillBox className="skillList"
            skillType="Testing Tools"
            skill1="Mocha"
            skill2="Jasmine"
